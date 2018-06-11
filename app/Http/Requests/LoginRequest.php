2b16fcdc532306beff7ca447d10579a1e3ec8d72
zmaren <36674206+zmaren@users.noreply.github.com>
2018-06-11 18:48:24 +0200

    Added modals, extended validation

--- app/Http/Requests/LoginRequest.php
+++ app/Http/Requests/LoginRequest.php
@@ -24,7 +24,6 @@ class LoginRequest extends FormRequest
     public function rules()
     {
         return [
-
             'email' => 'required|max:70|email',
             'password' => 'required',
         ];
---
+++ database/seeds/RoleSeed.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\Role;
+
+class RoleSeed extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Role::create([
+            'name' => 'dj',
+        ]);
+        Role::create([
+            'name' => 'party',
+        ]);
+    }
+}
