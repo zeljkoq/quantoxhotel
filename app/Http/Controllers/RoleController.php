2f58a862a3930dc37d01cfa6456b6008209079a5
zeljkoq <36674206+zeljkoq@users.noreply.github.com>
2018-06-13 00:30:35 +0200

    PHP doc

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -5,8 +5,15 @@ namespace App\Http\Controllers;
 use App\Models\Role;
 use Illuminate\Http\Request;
 
+/**
+ * Class RoleController
+ * @package App\Http\Controllers
+ */
 class RoleController extends Controller
 {
+    /**
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function getRoles()
     {
         $roles = Role::get()->toArray();
