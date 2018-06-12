e05224fcc4b194fb91aaaee03634cac7c428d02c
zmaren <marenovic.zeljko@gmail.com>
2018-06-13 00:48:53 +0200

    PHP doc

--- app/Models/Party.php
+++ app/Models/Party.php
@@ -4,8 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Party
+ * @package App\Models
+ */
 class Party extends Model
 {
+    /**
+     * @var string
+     */
     protected $table = 'parties';
 
 }
