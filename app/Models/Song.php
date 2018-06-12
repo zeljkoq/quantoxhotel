ab969af8322113465eb7d90e342212c9231ced15
zmaren <marenovic.zeljko@gmail.com>
2018-06-13 00:51:58 +0200

    PHP doc

--- app/Models/Song.php
+++ app/Models/Song.php
@@ -5,9 +5,16 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 
 
+/**
+ * Class Song
+ * @package App\Models
+ */
 class Song extends Model
 {
 
+    /**
+     * @var array
+     */
     protected $fillable = [
         'artist',
         'track',
@@ -16,19 +23,31 @@ class Song extends Model
         'updated_by'
     ];
 
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function user()
     {
         return $this->belongsTo(User::class);
     }
 
+    /**
+     * @var array
+     */
     protected $appends = ['delete', 'edit_index'];
 
 
+    /**
+     * @return string
+     */
     public function getEditIndexAttribute()
     {
         return route('song.edit.index', $this->id);
     }
 
+    /**
+     * @return string
+     */
     public function getDeleteAttribute()
     {
         return route('song.delete', $this->id);
