3ed915cef93d7c68e18f6004ecc2de62a664affd
Zeljko Marenovic <zeljko.marenovic@quantox.com>
2018-06-14 13:07:58 +0200

    Fixed PHP doc in migrations

--- database/migrations/2018_05_31_081940__create_permissions_table.php
+++ database/migrations/2018_05_31_081940__create_permissions_table.php
@@ -2,6 +2,10 @@
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+
+/**
+ * Class CreatePermissionsTable
+ */
 class CreatePermissionsTable extends Migration
 {
     /**
